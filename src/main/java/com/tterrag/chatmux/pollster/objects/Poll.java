360bb90309ada7da5f0eaf06d50a727c18075b57
tterrag <tterrag1098@gmail.com>
2020-11-10 13:25:07 -0500

    Remove interpreted_donation

--- src/main/java/com/tterrag/chatmux/pollster/objects/Poll.java
+++ src/main/java/com/tterrag/chatmux/pollster/objects/Poll.java
@@ -12,17 +12,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
-@RequiredArgsConstructor(onConstructor = @__({@JsonCreator}))
-@JsonIgnoreProperties(ignoreUnknown = true)
+@RequiredArgsConstructor(onConstructor = @__({ @JsonCreator }))
+@JsonIgnoreProperties("chat_event_uuid")
 public class Poll {
-    
+
     int id;
     String title;
-    
+
     @JsonProperty("start_time")
     Instant startTime;
-    @JsonProperty("interpreted_duration")
-    Duration duration;
+    String duration;
+    @JsonProperty("duration_seconds")
+    int durationSeconds;
     @JsonProperty("end_time")
     Instant endTime;
 
@@ -31,7 +32,7 @@ public class Poll {
     public String formatMessage() {
         StringBuilder ret = new StringBuilder(
                 title + " (#" + id + ")\n"
-                        + "From: " + startTime + " to " + endTime + " (" + duration + ")\n\n"
+                        + "From: " + startTime + " to " + endTime + " (" + Duration.ofSeconds(durationSeconds) + ")\n\n"
                         + "Choices:\n");
         for (Option option : options) {
             ret.append("  ").append(option.getTitle()).append(" (").append(option.getKey()).append("): ").append(option.getResults()).append(" votes\n");
