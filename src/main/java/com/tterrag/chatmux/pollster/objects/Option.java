9dde9aeba91a095f3b65f099ae687b0303b40d39
Stuart Pomeroy <stuart@pomeroys.site>
2023-11-03 21:43:17 +0000

    ignore extra properties we don't care about

--- src/main/java/com/tterrag/chatmux/pollster/objects/Option.java
+++ src/main/java/com/tterrag/chatmux/pollster/objects/Option.java
@@ -1,12 +1,14 @@
 package com.tterrag.chatmux.pollster.objects;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
 @RequiredArgsConstructor(onConstructor = @__({@JsonCreator}))
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Option {
 
     char key;
